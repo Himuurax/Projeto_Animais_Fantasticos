90c113a9d73f33161d9f06bda8246760eafbf161
Lucas Dominguez <luhcas_@live.com>
2021-03-27 14:25:52 -0300

    modal refatorado com class

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import TabNav from './modules/tab-nav.js';
 import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/animacao.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -21,8 +21,10 @@ accordion.init();
 const tabNav = new TabNav('.js-tabmenu li', '.js-tabcontent section');
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
+modal.init();
+
 initAnimacaoScroll();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
