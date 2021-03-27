a3e30d9ea578e3a5208fff789037256cd717cc2f
Lucas Dominguez <luhcas_@live.com>
2021-03-27 15:36:54 -0300

    tooltip refatorada

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import TabNav from './modules/tab-nav.js';
 import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/animacao.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -24,8 +24,10 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initAnimacaoScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
